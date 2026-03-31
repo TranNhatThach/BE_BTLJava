d4464b4ad6b616973e6f47decee293c5d2faaf4e
Vanitas-1006 <nhatthach1062006@gmail.com>
2026-03-31 13:58:21 +0700

    feat: implement class management service, evaluation repository, and database schema initialization

--- src/main/java/com/btljava/GiaSu/repository/DanhGiaRepository.java
+++ src/main/java/com/btljava/GiaSu/repository/DanhGiaRepository.java
@@ -2,7 +2,6 @@ package com.btljava.GiaSu.repository;
 
 import com.btljava.GiaSu.entity.DanhGia;
 import com.btljava.GiaSu.entity.LopHoc;
-import com.btljava.GiaSu.entity.TaiKhoan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +10,9 @@ import java.util.List;
 @Repository
 public interface DanhGiaRepository extends JpaRepository<DanhGia, Integer> {
     List<DanhGia> findByLopHoc_MaLop(Integer maLop);
+
     List<DanhGia> findByLopHoc_GiaSu_MaGiaSu(Integer maGiaSu);
+
     boolean existsByLopHoc_MaLop(Integer maLop);
 
     List<DanhGia> findByLopHoc(LopHoc lop);
